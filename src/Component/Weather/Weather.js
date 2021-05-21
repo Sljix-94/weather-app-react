9f5add23e2c052891a566ca621cc873ee7986959
Djordje Sljivancanin <djordje.sljivancanin@hotmail.com>
2021-05-21 14:39:58 +0200

    changed css code structure,background,set font

--- src/Component/Weather/Weather.js
+++ src/Component/Weather/Weather.js
@@ -4,13 +4,13 @@ const Weather = (props) => {
   return (
     <div className={classes.container}>
       <div className={classes.header}>
-        <h1>{props.weatherInfo.name}</h1>
+        <h1>Europe/{props.weatherInfo.name}</h1>
         <img
           src={`http://openweathermap.org/img/wn/${props.weatherInfo.icon}.png`}
           alt="Icon"
         />
       </div>
-      <div className={classes.temp}>{props.weatherInfo.temp} F</div>
+      <div className={classes.temperature}>{props.weatherInfo.temp} F</div>
       <div>{props.weatherInfo.description}</div>
     </div>
   );
